1283074032a733256bf6fb0c240a17f8f59f64b4
Dani Moll <47044961+danimoll@users.noreply.github.com>
2020-09-24 17:15:53 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,10 @@
 const meter = 100;
 const playerSpeed = 2;
 
+//Dani: I got an error related to the X and Y being undefined in the update function's if statement. I made them global variables for my particle/light effect too
+let x=100;
+let y=100;
+
 function setup() {
     createCanvas(1280, 800);
     background('black');
@@ -21,14 +25,17 @@ let player = {
         circle(this.x,this.y,50);
 
         //Give the player velocity towards a mouse
-        if(mouseIsPressed && int(dist(x=100,y=100,mouseX,mouseY)) > 5) {
-            this.x += ((mouseX-this.x)/meter)*playerSpeed;
-            this.y += ((mouseY-this.y)/meter)*playerSpeed;
+        if(mouseIsPressed && int(dist(x,y,mouseX,mouseY)) > 5) {
+            x = this.x += ((mouseX-this.x)/meter)*playerSpeed;
+            y = this.y += ((mouseY-this.y)/meter)*playerSpeed;
         }
+
+    //light beam! in progress
+      fill('yellow');
+      noStroke();
+      triangle(mouseX-10, mouseY-10, x, y, mouseX+10, mouseY+10);
     }
 }
-
-
 
 function draw() {
     player.update();
