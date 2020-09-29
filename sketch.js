df27605d85b44b49e6c9efe1eb41f38b60af9104
Jhorowitz0 <46899492+Jhorowitz0@users.noreply.github.com>
2020-09-29 17:00:36 -0400

    File cleaning
    arranged all lib files into a folder called libs

--- sketch.js
+++ sketch.js
@@ -8,36 +8,28 @@ world.addObject(new Wall(50, 50, 40, 70));
 world.addObject(new Wall(300,300,100,90));
 world.addObject(new Wall(100,150,20,100));
 
-
 var center; //used to situate eveything on the screen
+
 var player = new Player();//makes a new player object
+
+
+//------------------SETUP AND DRAW--------------------
 
 function setup() {
     createCanvas(windowWidth, windowWidth);
     background('black');
-    center = { x: windowWidth/2, y: windowHeight/2}; //csets center to the middle of screen
+    center = { x: windowWidth/2, y: windowHeight/2}; //sets center to the middle of screen
 }
 
 function draw() {
     background('black');
+    
     //faceReader.readFace(); //gets emotion from face on campera if there is one
     player.setEmotion(faceReader.getEmotionColor()); //updates player emotion color
+
     player.update(center); //updates player info
     player.draw(center); //draws the player on the screen
 
     world.update( player.getX(),player.getY() );
     world.draw();
-    // updateMap(player.getX(), player.getY());
 }
-
-
-// //Map Drawing functions
-// function updateMap(playerX, playerY) {
-//     push();
-//     translate(-playerX,-playerY);
-//     for( let i = 0; i < MapRects.length; i++) {
-//         MapRects[i].draw();
-//     }
-//     pop();
-// }
-
