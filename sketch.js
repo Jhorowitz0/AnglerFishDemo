9a28c3a032f8d703f5bd4878bd60d2bfae2b9eb0
Jhorowitz0 <46899492+Jhorowitz0@users.noreply.github.com>
2020-09-29 15:15:08 -0400

    Made Camera.js and FaceReader.js
    Camera.js takes care of webcam stuff and face api initialization
    
    facereader is an object used to get emotions from faces on the webcam

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 // webcam input stored here
-const video = document.getElementById("video");
+// const video = document.getElementById("video");
+
 
 //Map Variables
 var MapRects = [newWall(600,600,55,55),
@@ -25,80 +26,35 @@ const happyWeight = 150;
 const sadWeight = 200;
 const angryWeight = 300;
 
-//list of face-api models to load before using webcam input
-Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-    faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-    faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-    faceapi.nets.faceExpressionNet.loadFromUri("/models")
-]).then(startVideo);
 
-const contraints = {
-    video: true
-};
-
-function startVideo(){
-    console.log(navigator.appName);
-    navigator.mediaDevices.getUserMedia(
-        {video : true}
-    )
-    .then(function(stream) {
-        video.srcObject = stream;
-    })
-    .catch(function(err) {
-        console.log("you done fucked up");
-        console.log(err);
-    });
-}
-
-
-// async function startVideo(){
-//     let stream = null;
-//     const mediaD = navigator.mediaDevices;
-//     try {
-//         stream = await navigator.mediaDevices.getUserMedia(
-//             {video: true}
-//         );
-//         /* use the stream */
-//       } catch(err) {
-//           console.log(err);
-//       }
-//     video.srcObject = stream;
-// }
-
-//starts stream to video element, used after models are loaded
-// function startVideo() {
-//     Navigator.mediaDevices.getUserMedia( 
-//         {video: {} }, 
-//         stream => (video.srcObject = stream),
-//         err => console.log("you bitch")
-//     );
-// }
 	
 
 
 
-video.addEventListener("playing", () => {
-    // face-api detects faces every 100ms
-    setInterval( async () => {
-        var detections = await faceapi
-            .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()) //get all faces in view
-            .withFaceLandmarks()                                          //get 68 face landmarks
-            .withFaceExpressions();                                       //get scores for every expression
-            //possible expressions: neutral, happy, sad, angry, fearful
+// video.addEventListener("playing", () => {
+//     // face-api detects faces every 100ms
+//     setInterval( async () => {
+//         var detections = await faceapi
+//             .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()) //get all faces in view
+//             .withFaceLandmarks()                                          //get 68 face landmarks
+//             .withFaceExpressions();                                       //get scores for every expression
+//             //possible expressions: neutral, happy, sad, angry, fearful
 
-        //scale landmarks to match screen size 
-        //TODO: make an accurate mapping from webcam space to screenspace
-        detections = faceapi.resizeResults(detections, {width: windowWidth, height: windowHeight});
+//         //scale landmarks to match screen size 
+//         //TODO: make an accurate mapping from webcam space to screenspace
+//         detections = faceapi.resizeResults(detections, {width: windowWidth, height: windowHeight});
 
-        //set position and color if at least 1 face is detected
-        if(detections != null && detections[0] != null) {
-            position.x = windowWidth - Math.round(detections[0].landmarks.positions[0]._x);
-            position.y = Math.round(detections[0].landmarks.positions[0]._y);
-            expression_color = expressions_to_color(detections[0].expressions);
-        }
-    }, 1);
-})
+//         //set position and color if at least 1 face is detected
+//         if(detections != null && detections[0] != null) {
+//             position.x = windowWidth - Math.round(detections[0].landmarks.positions[0]._x);
+//             position.y = Math.round(detections[0].landmarks.positions[0]._y);
+//             expression_color = expressions_to_color(detections[0].expressions);
+//         }
+//     }, 1);
+// })
+
+var faceReader = new FaceReader();
+
 
 function setup() {
     createCanvas(windowWidth, windowWidth);
@@ -106,6 +62,7 @@ function setup() {
     center = { x: windowWidth/2, y: windowHeight/2};
     player.x = center.x;
     player.y = center.y;
+
 }
 
 // player class
@@ -139,7 +96,8 @@ var player = {
         //Dani:light beam!
         //expression_color is a combo of which expressions were detected
         noStroke();
-        fill(expression_color);
+        //fill(expression_color);
+        fill(faceReader.getEmotionColor());
 
         //beam originates on player and faces mouse position
         let angle = Math.atan2(mouseY-(center.x - this.size/2)
@@ -156,6 +114,7 @@ var player = {
 
 
 function draw() {
+    faceReader.readFace();
     background('black');
 
     player.update();
